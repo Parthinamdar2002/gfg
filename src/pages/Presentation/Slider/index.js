71d59aee7ba92fe7cff23936839cafa510d83108
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2023-09-04 17:20:05 +0530

    corousel

--- src/pages/Presentation/Slider/index.js
+++ src/pages/Presentation/Slider/index.js
@@ -26,11 +26,25 @@ function Slider() {
         <img src={arrowRight} alt="" />
         {/*eslint-disable*/}
       </div>
-      {[img1, img2, img3].map((cardState, i) => (
-        <div
-          style={{ backgroundImage: `url(${cardState})`, left: `${(-index + i) * 100}vw` }}
-          className={styles.card}
-        />
+      {[
+        { img: img1, text: "yo this is meow" },
+        { img: img2, text: "This is bhau" },
+        { img: img3, text: "This is bahin" },
+      ].map((cardState, i) => (
+        <>
+          <div
+            style={{ backgroundImage: `url(${cardState.img})`, top: `${(-index + i) * 100}vh` }}
+            className={styles.cardLeft}
+          >
+            <h1>{cardState.text}</h1>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${cardState.img})`, top: `${(index - i) * 100}vh` }}
+            className={styles.cardRight}
+          >
+            <h1>{cardState.text}</h1>
+          </div>
+        </>
       ))}
     </div>
   );
