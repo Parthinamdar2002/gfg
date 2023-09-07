22b1815c1d0e0b9f97d112e50242648f15d558f0
Hemant Singh Parmar <hemantsinghparmar@mgmutech.org>
2023-09-07 23:37:33 +0530

    updating  TeamData �

--- src/pages/LandingPages/AboutUs/sections/TeamData.js
+++ src/pages/LandingPages/AboutUs/sections/TeamData.js
@@ -1,6 +1,33 @@
-import Aniket from "public/img/1.jpg";
-import Sakshi from "public/img/ViceChair.jpg";
-import Parth from "public/img/ParthInamdar.jpg";
+import Aniket from "../../../../../public/img/1.jpg";
+import Sakshi from "../../../../../public/img/ViceChair.jpg";
+import Parth from "../../../../../public/img/ParthInamdar.jpg";
+import Ayush from "../../../../../public/img/DSC_4021.jpg";
+import Hemant from "../../../../../public/img/Hemant Singh Parmar.jpg";
+import Suraj from "../../../../../public/img/Snapchat-861277505.jpg";
+import Pratik from "../../../../../public/img/Pratik Jadhav.jpg";
+import ParthBhawar from "../../../../../public/img/Parth R. Bhawar.png";
+import Shashank from "../../../../../public/img/IMG_2843.jpg";
+import Avani from "../../../../../public/img/Avani Pathak.jpg";
+import Vivek from "../../../../../public/img/Vivek ingle.jpg"
+import Suhas from "../../../../../public/img/DSC_0292.jpg";
+import Aditi from "../../../../../public/img/Aditi Jadhav.img";
+import Kunal from "../../../../../public/img/Kunal Wagh.jpg";
+import Prajakta from "../../../../../public/img/";
+import Pratap from "../../../../../public/img/img.jpg";
+import Sushrut from "../../../../../public/img/Sushrut_Lekurwale_TeamPR.jpg";
+import SakshiAkhare from "../../../../../public/img/IMG_7444.heic";
+import Atharva from "../../../../../public/img/IMG_20230121_121344.jpg";
+import Priyesh from "../../../../../public/img/Priyesh.jpg";
+import Arya from "../../../../../public/img/Arya Malode.jpg";
+import Pruthavirajsingh from "../../../../../public/img/Pruthavirajsingh Dulat.jpg";
+import Pradyumn from "../../../../../public/img/pradyumn.jpeg.jpg";
+import Kartik from "../../../../../public/img/IMG_20230717_123512.jpg";
+import Swetal from "../../../../../public/img/Swetal.jpg";
+import Aakanksha from "../../../../../public/img/IMG_20230821_142027149~5.jpg";
+import Aditya from "../../../../../public/img/IMG_8408 2.jpg";
+
+
+
 
 const data = [
     {
@@ -49,6 +76,7 @@ const data = [
       "Sr. no.": "4",
       "Name of team member": "Ayush Chaudhari ",
       "Designation": "Member",
+      "img": Ayush,
       "Team": "",
       "Contact number": "9325898195",
       "Email address": "ayushschaudhari1904@gmail.com",
@@ -62,6 +90,7 @@ const data = [
       "Sr. no.": "5",
       "Name of team member": "Hemant Singh Parmar",
       "Designation": "Member",
+      "img": Hemant,
       "Team": "",
       "Contact number": "7559228490",
       "Email address": "anmolpawar877@gmail.com",
@@ -75,6 +104,7 @@ const data = [
       "Sr. no.": "6",
       "Name of team member": "Suraj Nagre ",
       "Designation": "Member",
+      "img": Suraj,
       "Team": "",
       "Contact number": "7558697774",
       "Email address": "surajnagre30@gmail.com",
@@ -88,6 +118,7 @@ const data = [
       "Sr. no.": "7",
       "Name of team member": "Pratik Jadhav",
       "Designation": "Member",
+      "img":Pratik,
       "Team": "",
       "Contact number": "9404730294",
       "Email address": "j.pratik025@gmail.com",
@@ -100,6 +131,7 @@ const data = [
     {
       "Sr. no.": "8",
       "Name of team member": "Parth Bhawar",
+      "img":ParthBhawar,
       "Designation": "Member",
       "Team": "",
       "Contact number": "9373097450",
@@ -114,6 +146,7 @@ const data = [
       "Sr. no.": "9",
       "Name of team member": "Shashank Padhye",
       "Designation": "Head",
+      "img":Shashank,
       "Team": "Events",
       "Contact number": "9356425499",
       "Email address": "shashankpadhye9@gmail.com",
@@ -127,6 +160,7 @@ const data = [
       "Sr. no.": "10",
       "Name of team member": "Avani Pathak ",
       "Designation": "Member",
+      "img":Avani,
       "Team": "",
       "Contact number": "9021999704",
       "Email address": "pathakavani0226@gmail.com",
@@ -140,6 +174,7 @@ const data = [
       "Sr. no.": "11",
       "Name of team member": "Vivek Ingle",
       "Designation": "Member",
+      "img":Vivek,
       "Team": "",
       "Contact number": "9529743301",
       "Email address": "vivekingle315@gmail.com",
@@ -153,6 +188,7 @@ const data = [
       "Sr. no.": "12",
       "Name of team member": "Suhas Magar",
       "Designation": "Member",
+      "img":Suhas,
       "Team": "",
       "Contact number": "7020463624",
       "Email address": "suhasmagar273@gmail.com",
@@ -166,6 +202,7 @@ const data = [
       "Sr. no.": "13",
       "Name of team member": "Aditi Jadhav",
       "Designation": "Member",
+      "img":Aditi,
       "Team": "",
       "Contact number": "7773923224",
       "Email address": "jadhavaditi126@gmail.com",
@@ -179,6 +216,7 @@ const data = [
       "Sr. no.": "14",
       "Name of team member": "Kunal Wagh",
       "Designation": "Member",
+      "img":Kunal,
       "Team": "",
       "Contact number": "9822634213",
       "Email address": "kunalwagh282@gmail.com",
@@ -192,6 +230,7 @@ const data = [
       "Sr. no.": "15",
       "Name of team member": "Prajakta Wakhare",
       "Designation": "Head",
+      "img":Prajakta,
       "Team": "Design",
       "Contact number": "8055860877",
       "Email address": "wakhareprajakta@gmail.com",
@@ -203,8 +242,9 @@ const data = [
     },
     {
       "Sr. no.": "16",
-      "Name of team member": "pratap Pawar",
+      "Name of team member": "Pratap Pawar",
       "Designation": "Member",
+      "img":Pratap,
       "Team": "",
       "Contact number": "9309058717",
       "Email address": "pratapsingh08pawar@gmail.com",
@@ -243,6 +283,7 @@ const data = [
     {
       "Sr. no.": "19",
       "Name of team member": "Sushrut Lekurwale",
+      "img":Sushrut,
       "Designation": "Head",
       "Team": "Public Relations ",
       "Contact number": "7768882081",
@@ -257,6 +298,7 @@ const data = [
       "Sr. no.": "20",
       "Name of team member": "Sakshi Akhare",
       "Designation": "Member",
+      "img":SakshiAkhare,
       "Team": "",
       "Contact number": "9921096415",
       "Email address": "sakshiakhare17@gmail.com",
@@ -270,6 +312,7 @@ const data = [
       "Sr. no.": "21",
       "Name of team member": "Atharva Raulwar",
       "Designation": "Member",
+      "img":Atharva,
       "Team": "",
       "Contact number": "7507975334",
       "Email address": "atharvateamxtreme@gmail.com",
@@ -283,6 +326,7 @@ const data = [
       "Sr. no.": "22",
       "Name of team member": "Priyesh Sahijwani ",
       "Designation": "Member",
+      "img":Priyesh,
       "Team": "",
       "Contact number": "9890532718",
       "Email address": "priyeshsahijwani@gmail.com",
@@ -296,6 +340,7 @@ const data = [
       "Sr. no.": "23",
       "Name of team member": "Arya Malode",
       "Designation": "Head",
+      "img":Arya,
       "Team": "Social Media",
       "Contact number": "8767467058",
       "Email address": "aryamalode17@gmail.com",
@@ -309,6 +354,7 @@ const data = [
       "Sr. no.": "24",
       "Name of team member": "Pruthavirajsingh Dulat",
       "Designation": "Member",
+      "img":Pruthavirajsingh,
       "Team": "",
       "Contact number": "8793879208",
       "Email address": "pruthvirajytube@gmail.com",
@@ -322,6 +368,7 @@ const data = [
       "Sr. no.": "25",
       "Name of team member": "Pradyumn Bhanegaonkar ",
       "Designation": "Member",
+      "img":Pradyumn,
       "Team": "",
       "Contact number": "9422886711",
       "Email address": "pradyumnbhanegaonkar@gmail.com",
@@ -335,6 +382,7 @@ const data = [
       "Sr. no.": "26",
       "Name of team member": "Kartik Mhaske",
       "Designation": "Head",
+      "img":Kartik,
       "Team": "Marketing",
       "Contact number": "9130373357",
       "Email address": "Kartik69personal@gmail.com ",
@@ -348,6 +396,7 @@ const data = [
       "Sr. no.": "27",
       "Name of team member": "Swetal Matkar",
       "Designation": "Member",
+      "img":Swetal,
       "Team": "",
       "Contact number": "8007773577",
       "Email address": "Matkarswetal@gmail.com",
@@ -361,6 +410,7 @@ const data = [
       "Sr. no.": "28",
       "Name of team member": "Aakanksha Kate",
       "Designation": "Member",
+      "img":Aakanksha,
       "Team": "",
       "Contact number": "9168839562",
       "Email address": "aakankshakate29@gmail.com ",
@@ -374,6 +424,7 @@ const data = [
       "Sr. no.": "29",
       "Name of team member": "Aditya Isadkar",
       "Designation": "Member",
+      "img":Aditya,
       "Team": "",
       "Contact number": "9403412529",
       "Email address": "adityaisadkar940@gmail.com",
